8b0c8120a20387833d857abbc50e59411345d860
九反 佑樹 <kid@jiuhan-no-MacBook-Air.local>
2017-09-10 22:00:34 +0900

    Some comment.

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+    "github.com/gin-gonic/gin"
+    "github.com/line/line-bot-sdk-go/linebot"  // ① SDKを追加
 )
 
 func main() {
-	port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+    // ② LINE bot instanceの作成
+    bot, err := linebot.New(
+        os.Getenv("CHANNEL_SECRET"),
+        os.Getenv("CHANNEL_TOKEN"),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
 
-	router.Run(":" + port)
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    // ③ LINE Messaging API用の Routing設定
+    router.POST("/callback", func(c *gin.Context) {
+        events, err := bot.ParseRequest(c.Request)
+        if err != nil {
+            if err == linebot.ErrInvalidSignature {
+                log.Print(err)
+            }
+            return
+        }
+        for _, event := range events {
+            if event.Type == linebot.EventTypeMessage {
+                switch message := event.Message.(type) {
+                case *linebot.TextMessage:
+                    if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+                        log.Print(err)
+                    }
+                }
+            }
+        }
+    })
+
+    router.Run(":" + port)
 }
